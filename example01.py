ec853e5eb26d5ed6039ff5291c30abefbd1ef006
J. Lucas Grillo <jlucas.gl@gmail.com>
2017-07-23 16:23:29 +0100

    Improved getters/setters
    Updated example01

--- example01.py
+++ example01.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
 from Uarm import Uarm
+from sys import platform
 
-brazo = Uarm('COM7', baudrate = 9600)
+if platform == "win32":
+    brazo = Uarm('COM7', baudrate = 9600)
+else:
+    brazo = Uarm('/dev/tty0', baudrate = 9600)
 brazo.move(10, 20, 30)
 brazo.move(100, 200, 300)
 brazo.move(1000, 2000, 3000)
-brazo.set_speed(4500)
-velocidad = brazo.get_speed()
+brazo.speed = 4500
+velocidad = brazo.speed
 print(str(velocidad))
 brazo.close()
-
-
